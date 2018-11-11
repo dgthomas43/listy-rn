e0d4ff6cd93cea8e13c215442da218d0cd587468
dgthomas43 <dgthomas43@gmail.com>
2018-11-11 12:16:14 -0500

    Linked react-native-splash-screen and added splas screen for android

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import Main from './src/components/main/main'
+import Main from './src/components/main/main';
+import SplashScreen from 'react-native-splash-screen';
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+  
   render() {
     return (
       <Main />
